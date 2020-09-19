7b2679e2836531bf866d75dc4cf2ed29a31893ba
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-18 19:44:56 -0700

    added handlebars

---
+++ public/burgers.js
@@ -0,0 +1,49 @@
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+    
+      var burgerDevoured = {
+        devoured: true
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: burgerDevoured
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".add-burger").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      let burgerName=$("#burger-name").val().trim();
+       if(burgerName=' ' ){
+           return false;
+       }
+
+      var newBurger = {
+        name: burgerName,
+        devoured: false
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  });
+  
